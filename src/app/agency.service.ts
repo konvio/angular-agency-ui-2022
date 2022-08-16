fe3cd121b8753f1729145241a6a78940b59995c3
Vitaliy Kononenko <v@kononenko.tech>
2022-08-16 08:32:49 +0300

    Implement AgencyService

--- src/app/agency.service.ts
+++ src/app/agency.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Agency } from "./agency";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AgencyService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/api/v1/agencies';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAllAgencies(): Observable<Agency[]> {
+    return this.http.get<Agency[]>(this.baseUrl);
+  }
+
+  createOrUpdateAgency(agency: Agency): Observable<Agency> {
+    return this.http.put<Agency>(this.baseUrl, agency);
+  }
+
+  deleteAgency(id: string): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
 }
---
+++ src/app/agency.ts
@@ -0,0 +1,10 @@
+export interface Agency {
+  id: string,
+  name: string,
+  country: string,
+  countryCode: string,
+  city: string,
+  street: string,
+  settlementCurrency: string,
+  contactPerson: string
+}
